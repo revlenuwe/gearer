8150c82814bb9dbe47280ca46ca9199f62b9218e
Bruno Farias <brunofarias1@gmail.com>
2021-03-22 20:29:11 -0300

    code review

--- config/config.php
+++ config/config.php
@@ -6,6 +6,7 @@ return [
 	| MyceliumGear authorization configuration
 	|--------------------------------------------------------------------------
     */
+    'gateway_url' => env('GEAR_GATEWAY_URL', ''),
     'gateway_id' => env('GEAR_GATEWAY_ID',''),
     'gateway_secret' => env('GEAR_GATEWAY_SECRET',''),
 
